be20e6a17d332fb5a849c45548ee078971fdd052
Abhinav Raghunathan <abhinavr2121@gmail.com>
2021-08-19 11:38:15 -0400

    Latest content update

--- import_data.py
+++ import_data.py
@@ -11,7 +11,7 @@ init(autoreset = True)
 client = pymongo.MongoClient("mongodb+srv://normal-user:7Pv02lHlo3Dzdxai@cluster0.ro0mt.mongodb.net/RacquetStats?retryWrites=true&w=majority")
 db = client.RacquetStats
 
-last_updated = '2021-05-30'
+last_updated = '2021-08-15'
 
 print('Finding new matches since ' + last_updated + '...')
 
